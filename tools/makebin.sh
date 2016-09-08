afaaad56f09adbdc5f65f819708e8259c7aed3eb
Yasushi Ichikawa <ichikaway@gmail.com>
2016-09-08 17:52:26 +0900

    change win bin name using .exe

--- tools/makebin.sh
+++ tools/makebin.sh
@@ -5,6 +5,6 @@ cd ../src/vaddy/
 go build  -o ../../bin/vaddy-macosx64
 GOOS=linux GOARCH=386 go build  -o ../../bin/vaddy-linux-32bit
 GOOS=linux GOARCH=amd64 go build  -o ../../bin/vaddy-linux-64bit
-GOOS=windows GOARCH=386 go build  -o ../../bin/vaddy-win-32bit
-GOOS=windows GOARCH=amd64 go build  -o ../../bin/vaddy-win-64bit
+GOOS=windows GOARCH=386 go build  -o ../../bin/vaddy-win-32bit.exe
+GOOS=windows GOARCH=amd64 go build  -o ../../bin/vaddy-win-64bit.exe
 
