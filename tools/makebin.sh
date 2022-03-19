484f49b7dbddb302d627a55154e738dd0669655f
Yasushi Ichikawa <ichikaway@gmail.com>
2022-03-19 11:39:53 +0900

    ver1.1.0 update exe bin
    to specify scan type option
    set go-vaddy client version on UserAgent
    set api key on http header

--- tools/makebin.sh
+++ tools/makebin.sh
@@ -8,4 +8,4 @@ GOOS=linux GOARCH=amd64 go build  -o ../../bin/vaddy-linux-64bit
 #GOOS=windows GOARCH=386 go build  -o ../../bin/vaddy-win-32bit.exe
 GOOS=windows GOARCH=amd64 go build  -o ../../bin/vaddy-win-64bit.exe
 #GOOS=freebsd GOARCH=386 go build  -o ../../bin/vaddy-freebsd-32bit
-#GOOS=freebsd GOARCH=amd64 go build  -o ../../bin/vaddy-freebsd-64bit
+GOOS=freebsd GOARCH=amd64 go build  -o ../../bin/vaddy-freebsd-64bit
